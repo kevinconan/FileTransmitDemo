17c7acbf28766ca08672164dc5e2fbfcdc34244e
kevinconan <kevinlyhan@gmail.com>
2014-12-06 16:37:28 +0800

    1

---
+++ FileTransmitDemo/NetProtocal.common/IMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetProtocal.Common
+{
+    public abstract class IMessage
+    {
+        public CommandType CommandType { get; set; }
+        public object Content { get; set; }
+
+    }
+
+    public enum CommandType : int { 
+        HeartBeats = 0,
+        Login = 1,
+        Logout = 2,
+        Response = 3,
+        File = 4
+    }
+}
